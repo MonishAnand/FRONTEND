022ed3deedfb45d8887939a760cb0a1109fb11b9
monish <monishanand.j@gmail.com>
2023-09-06 22:24:21 +0530

    wasted

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,14 @@
 console.log("hello world  bla❤️")
-a= prompt("enter your name")
+a= 12
 console.log(a)
+/*const arr=[22,18,14,15,16]
+for( i=0;i<5;i++){
+    if(arr[i]%2==0) {
+        console.log(arr[i]);
+    }
+    
+}*/
+function b (){
+    console.log("hi");
+}
+b ();
